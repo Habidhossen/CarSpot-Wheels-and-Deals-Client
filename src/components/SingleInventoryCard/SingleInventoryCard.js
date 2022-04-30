e83187a4ace573704a1fe7fd72c1eab523951549
Habidhossen <habidhossen2@gmail.com>
2022-05-01 03:14:52 +0600

    card updated

--- src/components/SingleInventoryCard/SingleInventoryCard.js
+++ src/components/SingleInventoryCard/SingleInventoryCard.js
@@ -9,18 +9,18 @@ const SingleInventoryCard = ({ car }) => {
     <div className="col">
       <div className="inventory-card">
         <img className="inventory-card-img" src={image} alt="" />
+        <h5 className="inventory-card-supplier">
+          Supplier: <span>{supplier}</span>
+        </h5>
+        <h1 className="inventory-card-title">{name}</h1>
+        <h3 className="inventory-card-price">$ {price}</h3>
+        <p className="inventory-card-desc">{desc}</p>
         <div>
           <h5 className="inventory-card-quantity">
             Quantity: <b>{quantity}</b>{" "}
             <span className="badge ms-1">In stock</span>
           </h5>
         </div>
-        <h1 className="inventory-card-title">{name}</h1>
-        <h3 className="inventory-card-price">$ {price}</h3>
-        <p className="inventory-card-desc">{desc}</p>
-        <h5 className="inventory-card-supplier">
-          Supplier: <span>{supplier}</span>
-        </h5>
         <div className="d-flex justify-content-center">
           <Link to="" className="inventory-card-btn">
             Stock Update
