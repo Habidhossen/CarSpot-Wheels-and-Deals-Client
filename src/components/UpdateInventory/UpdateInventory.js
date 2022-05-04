a0c63f07e6321ff6fe0e966241484d143c264d2a
Habidhossen <habidhossen2@gmail.com>
2022-05-05 02:29:15 +0600

    delived and restock quantity added

--- src/components/UpdateInventory/UpdateInventory.js
+++ src/components/UpdateInventory/UpdateInventory.js
@@ -5,27 +5,50 @@ const UpdateInventory = () => {
   const { inventoryID } = useParams();
   const [inventory, setInventory] = useState({});
 
+  // load data
   useEffect(() => {
     const url = `http://localhost:5000/inventory/${inventoryID}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setInventory(data));
-  }, []);
+  }, [inventory]);
 
   // handle quantity delivered button
   const handleDeliveredBtn = () => {
-    const quantity = inventory.quantity;
+    const quantity = parseInt(inventory.quantity) - 1; //old-quantity - 1
+    const updatedQuantity = { quantity }; //create quantity object
 
-    const newQuantity = quantity - 1;
-
-    console.log(newQuantity);
+    // update quantity
+    fetch(`http://localhost:5000/inventory/${inventoryID}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedQuantity),
+    })
+      .then((res) => res.json())
+      .then((data) => {});
   };
 
   // handle restock quantity
   const handleReStock = (event) => {
     event.preventDefault();
-    const value = event.target.quantity.value;
-    console.log(value);
+    const value = event.target.quantity.value; //input value
+    const quantity = parseInt(inventory.quantity) + parseInt(value); //old-quantity + input value
+    const updatedQuantity = { quantity }; //create quantity object
+
+    // update quantity
+    fetch(`http://localhost:5000/inventory/${inventoryID}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedQuantity),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        event.target.reset();
+      });
   };
 
   return (
