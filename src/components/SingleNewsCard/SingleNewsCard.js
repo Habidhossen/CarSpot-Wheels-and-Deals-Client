1d12d11e7149243df8db394ca16bf13ab5914d06
Habidhossen <habidhossen2@gmail.com>
2022-05-06 02:01:32 +0600

    design updated

--- src/components/SingleNewsCard/SingleNewsCard.js
+++ src/components/SingleNewsCard/SingleNewsCard.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./SingleNewsCard.css";
 
 const SingleNewsCard = ({ news }) => {
-  const { title, desc, image } = news;
+  const { title, desc, date, image } = news;
 
   return (
     <div className="col">
       <div className="news-card">
         <img className="news-card-img" src={image} alt="" />
         <div className="p-3">
+          <p className="news-card-sub-title">{date}</p>
           <h1 className="news-card-title">{title}</h1>
-          <p className="news-card-text">{desc}</p>
+          <p className="news-card-text">
+            {desc}...<span>Read more</span>
+          </p>
         </div>
       </div>
     </div>
